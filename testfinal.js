33b7685bdd21bda314179f8f2a869331eebdf711
mundra22 <44059307+mundra22@users.noreply.github.com>
2020-08-23 00:05:01 +0530

    Update testfinal.js

--- testfinal.js
+++ testfinal.js
@@ -1,28 +1,14 @@
 (function () {
+    var imageField;
     metadata = {
-      "systemName": "FinovVahan.jssp",
-      "displayName": "FinovVahan.jssp",
-      "description": "A K2 JSSP Broker for Vahan Integration",
+      "systemName": "LeadSquaredDocumentUrl.jssp",
+      "displayName": "LeadSquaredDocumentUrlJssp",
+      "description": "A K2 JSSP Broker for leadsquared",
       "configuration": {
-        "grant_type": {
-          "displayName": "grant type",
-          "type": "string"
-        },
-        "client_id": {
-          "displayName": "client id",
-          "type": "string"
-        },
-        "client_secret": {
-          "displayName": "client secret",
-          "type": "string"
-        },
-        "username": {
-          "displayName": "username",
-          "type": "string"
-        },
-        "password": {
-          "displayName": "password",
-          "type": "string"
+        "ServiceURL": {
+          "displayName": "Service URL",
+          "type": "string",
+          "value": "https://api-in21.leadsquared.com"
         }
       }
     };
@@ -32,131 +18,51 @@
     }) {
       postSchema({
         "objects": {
-          "VahaanIntegration": {
-            "displayName": "Vahaan Integration",
-            "description": "Vahaan Integration",
+          "body": {
+            "displayName": "body",
+            "description": "payload",
             "properties": {
-              "token": {
-                "displayName": "token",
-                "type": "string",
-                "description": "auth token Value"
+              "ActivityCode": {
+                "displayName": "ActivityCode",
+                "type": "number",
+                "description": "Parameter Value"
               },
-              "message": {
-                "displayName": "message",
-                "type": "String",
-                "description": "message of call"
-              },
-              "status": {
-                "displayName": "status",
-                "type": "String",
-                "description": "status of call"
-              },
-              "registeringAuthority": {
-                "displayName": "registering Authority",
-                "type": "String",
-                "description": "registeringAuthority of call"
-              },
-              "registrationNo": {
-                "displayName": "registration No",
-                "type": "String",
-                "description": "registrationNo of call"
-              },
-              "registrationDate": {
-                "displayName": "registrationDate",
-                "type": "String",
-                "description": "registrationDate of call"
-              },
-              "ownerName": {
-                "displayName": "owner Name",
-                "type": "String",
-                "description": "owner name of call"
-              },
-              "fitnessUpto": {
-                "displayName": "fitness Upto",
-                "type": "String",
-                "description": "fitnessUpto of call"
-              },
-              "taxUpto": {
-                "displayName": "taxUpto",
-                "type": "String",
-                "description": "taxUpto of call"
-              },
-              "rcStatus": {
-                "displayName": "rcStatus",
-                "type": "String",
-                "description": "rcStatus of call"
-              },
-              "nocDetails": {
-                "displayName": "nocDetails",
-                "type": "String",
-                "description": "nocDetails of call"
-              },
-              "financierName": {
-                "displayName": "financierName",
-                "type": "String",
-                "description": "financierName of call"
-              },
-              "financed": {
-                "displayName": "financed",
-                "type": "String",
-                "description": "financed of call"
-              },
-              "blackListStatus": {
-                "displayName": "blackListStatus",
-                "type": "String",
-                "description": "blackListStatus of call"
-              },
-              "permit": {
-                "displayName": "permit",
-                "type": "String",
-                "description": "permit of call"
-              },
-              "insuranceName": {
-                "displayName": "insuranceName",
-                "type": "String",
-                "description": "insuranceName of call"
-              },
-              "policyNo": {
-                "displayName": "policyNo",
-                "type": "String",
-                "description": "policyNo of call"
-              },
-              "validUpto": {
-                "displayName": "validUpto",
-                "type": "String",
-                "description": "validUpto of call"
+              "result": {
+                "displayName": "result",
+                "type": "extendedString",
+                "description": "Result of call"
               }
             },
             "methods": {
-              "getVahaanToken": {
-                "displayName": "Get Vahaan Token",
-                "type": "read",
+              "retrieveFileUrl": {
+                "displayName": "retrieveFileUrl",
+                "type": "create",
+                "inputs": ["ActivityCode"],
+                "requiredInputs": ["ActivityCode"],
                 "parameters": {
-                  "toke_url": {
-                    "displayName": "toke url",
-                    "type": "string"
-                  }
-                },
-                "requiredParameters": ["toke_url"],
-                "outputs": ["token"]
-              },
-              "checkAPIMethod": {
-                "displayName": "CheckAPIMethod",
-                "type": "read",
-                "parameters": {
-                  "methodUrl": {
-                    "displayName": "methodUrl",
-                    "description": "Method Url",
+                  "accessKey": {
+                    "displayName": "accessKey",
                     "type": "string"
                   },
-                  "authToken": {
-                    "displayName": "authToken",
-                    "description": "Auth Token",
+                  "secretKey": {
+                    "displayName": "secretKey",
+                    "type": "string"
+                  },
+                  "leadId": {
+                    "displayName": "leadId",
+                    "type": "string"
+                  },
+                  "getFileUrl": {
+                    "displayName": "getFileUrl",
+                    "type": "string"
+                  },
+                  "fieldName": {
+                    "displayName": "fieldName",
                     "type": "string"
                   }
                 },
-                "requiredParameters": ["methodUrl", "authToken"],
-                "outputs": ["message", "status", "registeringAuthority", "registrationNo", "registrationDate", "ownerName", "fitnessUpto", "taxUpto", "rcStatus", "nocDetails", "financierName", "financed", "blackListStatus", "permit", "insuranceName", "policyNo", "validUpto"]
+                "requiredParameters": ["accessKey", "secretKey", "leadId", "getFileUrl", "fieldName"],
+                "outputs": ["result"]
               }
             }
           }
@@ -172,8 +78,8 @@
       configuration
     }) {
       switch (objectName) {
-        case "VahaanIntegration":
-          await onexecuteVahaanIntegration(methodName, parameters, properties, configuration);
+        case "body":
+          await onexecutebody(methodName, parameters, properties, configuration);
           break;
 
         default:
@@ -181,14 +87,10 @@
       }
     };
 
-    async function onexecuteVahaanIntegration(methodName, parameters, properties, configuration) {
+    async function onexecutebody(methodName, parameters, properties, configuration) {
       switch (methodName) {
-        case "getVahaanToken":
-          await onexecuteVahaanIntegrationgetVahaanToken(parameters, properties, configuration);
-          break;
-
-        case "checkAPIMethod":
-          await onexecuteVahaanIntegrationcheckAPIMethod(parameters);
+        case "retrieveFileUrl":
+          await onexecutebodyretrieveFileUrl(parameters, properties, configuration);
           break;
 
         default:
@@ -196,19 +98,27 @@
       }
     }
 
-    function onexecuteVahaanIntegrationgetVahaanToken(parameters, properties, configuration) {
+    function onexecutebodyretrieveFileUrl(parameters, properties, configuration) {
       return new Promise((resolve, reject) => {
-        let urlValue = parameters["toke_url"];
-        let httpPath = `grant_type=${configuration["grant_type"]}&client_id=${configuration["client_id"]}&client_secret=${configuration["client_secret"]}&username=${configuration["username"]}&password=${configuration["password"]}`;
+        let urlValue = configuration["ServiceURL"];
+        let httpPath = `/v2/ProspectActivity.svc/Retrieve?accessKey=${parameters["accessKey"]}&secretKey=${parameters["secretKey"]}&leadId=${parameters["leadId"]}&getFileUrl=${parameters["getFileUrl"]}`;
+        let ActivityCode = properties["ActivityCode"];
+        let data = {
+          "Parameter": {
+            "ActivityEvent": ActivityCode
+          }
+        };
         let xhr = new XMLHttpRequest();
 
-        xhr.onreadystatechange = function () {
+        xhr.onreadystatechange = function (parameters) {
           try {
             if (xhr.readyState !== 4) return;
             if (xhr.status !== 200 && xhr.status !== 201) throw new Error("Failed with status " + xhr.status);
             let obj = JSON.parse(xhr.responseText);
+            imageField = parameters["fieldName"];
+            let mxCustomObj = JSON.parse(obj.ProspectActivities[0].ActivityFields[imageField]);
             postResult({
-              "token": obj.access_token
+              "result": mxCustomObj.mx_CustomObject_1
             });
             resolve();
           } catch (error) {
@@ -216,54 +126,12 @@
           }
         };
 
+        urlValue = urlValue.endsWith("/") ? urlValue : urlValue + "/";
+        httpPath = httpPath.startsWith("/") ? httpPath.substr(1) : httpPath + "/";
         xhr.withCredentials = false;
-        xhr.open("post", urlValue + "?" + httpPath);
-        xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-        xhr.send();
-      });
-    }
-
-    function onexecuteVahaanIntegrationcheckAPIMethod(parameters, properties, configuration) {
-      return new Promise((resolve, reject) => {
-        let urlValue = parameters["methodUrl"];
-        let httpPath = parameters["authToken"];
-        let xhr = new XMLHttpRequest();
-
-        xhr.onreadystatechange = function () {
-          try {
-            if (xhr.readyState !== 4) return;
-            if (xhr.status !== 200 && xhr.status !== 201) throw new Error("Failed with status " + xhr.status);
-            let obj = JSON.parse(xhr.responseText);
-            postResult({
-              "message": obj.message,
-              "status": obj.status,
-              "registeringAuthority": obj.registeringAuthority,
-              "registrationNo": obj.registrationNo,
-              "registrationDate": obj.registrationDate,
-              "ownerName": obj.ownerName,
-              "fitnessUpto": obj.fitnessUpto,
-              "taxUpto": obj.taxUpto,
-              "rcStatus": obj.rcStatus,
-              "nocDetails": obj.nocDetails,
-              "financierName": obj.financierName,
-              "financed": obj.financed,
-              "blackListStatus": obj.blackListStatus,
-              "permit": obj.permit,
-              "insuranceName": obj.insuranceDetails.insuranceName,
-              "policyNo": obj.insuranceDetails.policyNo,
-              "validUpto": obj.insuranceDetails.validUpto
-            });
-            resolve();
-          } catch (error) {
-            reject(error);
-          }
-        };
-
-        xhr.withCredentials = false;
-        xhr.open("get", urlValue);
+        xhr.open("post", urlValue + httpPath);
         xhr.setRequestHeader("Content-Type", "application/json");
-        xhr.setRequestHeader("Authorization", "Bearer " + httpPath);
-        xhr.send();
+        xhr.send(JSON.stringify(data));
       });
     }
 
