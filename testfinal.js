98694f06663c6a6612a2af73c1e41810e3544216
mundra22 <44059307+mundra22@users.noreply.github.com>
2020-08-22 22:35:38 +0530

    Update testfinal.js

--- testfinal.js
+++ testfinal.js
@@ -1,13 +1,28 @@
 (function () {
     metadata = {
-      "systemName": "jssp.whatsapp",
-      "displayName": "Whatsapp JSSP",
-      "description": "A K2 JSSP Broker to send Whatsapp Messages",
+      "systemName": "FinovVahan.jssp",
+      "displayName": "FinovVahan.jssp",
+      "description": "A K2 JSSP Broker for Vahan Integration",
       "configuration": {
-        "ServiceURL": {
-          "displayName": "Service URL",
-          "type": "string",
-          "value": "https://pwpynv.api.infobip.com/omni/1"
+        "grant_type": {
+          "displayName": "grant type",
+          "type": "string"
+        },
+        "client_id": {
+          "displayName": "client id",
+          "type": "string"
+        },
+        "client_secret": {
+          "displayName": "client secret",
+          "type": "string"
+        },
+        "username": {
+          "displayName": "username",
+          "type": "string"
+        },
+        "password": {
+          "displayName": "password",
+          "type": "string"
         }
       }
     };
@@ -16,49 +31,132 @@
       configuration
     }) {
       postSchema({
-        objects: {
-          message: {
-            displayName: "message",
-            description: "message",
-            properties: {
-              phonenr: {
-                displayName: "PhoneNr",
-                type: "string",
-                description: "The Destination Phone nr"
+        "objects": {
+          "VahaanIntegration": {
+            "displayName": "Vahaan Integration",
+            "description": "Vahaan Integration",
+            "properties": {
+              "token": {
+                "displayName": "token",
+                "type": "string",
+                "description": "auth token Value"
               },
-              scenarioKey: {
-                displayName: "scenarioKey",
-                type: "string",
-                description: "scenarioKey Value"
+              "message": {
+                "displayName": "message",
+                "type": "String",
+                "description": "message of call"
               },
-              templateName: {
-                displayName: "templateName",
-                type: "string",
-                description: "template Name"
+              "status": {
+                "displayName": "status",
+                "type": "String",
+                "description": "status of call"
               },
-              templateVariables: {
-                displayName: "templateVariables",
-                type: "array",
-                description: "Please provide comma seperated string values"
+              "registeringAuthority": {
+                "displayName": "registering Authority",
+                "type": "String",
+                "description": "registeringAuthority of call"
               },
-              result: {
-                displayName: "Result",
-                type: "string",
-                description: "Result of call"
+              "registrationNo": {
+                "displayName": "registration No",
+                "type": "String",
+                "description": "registrationNo of call"
+              },
+              "registrationDate": {
+                "displayName": "registrationDate",
+                "type": "String",
+                "description": "registrationDate of call"
+              },
+              "ownerName": {
+                "displayName": "owner Name",
+                "type": "String",
+                "description": "owner name of call"
+              },
+              "fitnessUpto": {
+                "displayName": "fitness Upto",
+                "type": "String",
+                "description": "fitnessUpto of call"
+              },
+              "taxUpto": {
+                "displayName": "taxUpto",
+                "type": "String",
+                "description": "taxUpto of call"
+              },
+              "rcStatus": {
+                "displayName": "rcStatus",
+                "type": "String",
+                "description": "rcStatus of call"
+              },
+              "nocDetails": {
+                "displayName": "nocDetails",
+                "type": "String",
+                "description": "nocDetails of call"
+              },
+              "financierName": {
+                "displayName": "financierName",
+                "type": "String",
+                "description": "financierName of call"
+              },
+              "financed": {
+                "displayName": "financed",
+                "type": "String",
+                "description": "financed of call"
+              },
+              "blackListStatus": {
+                "displayName": "blackListStatus",
+                "type": "String",
+                "description": "blackListStatus of call"
+              },
+              "permit": {
+                "displayName": "permit",
+                "type": "String",
+                "description": "permit of call"
+              },
+              "insuranceName": {
+                "displayName": "insuranceName",
+                "type": "String",
+                "description": "insuranceName of call"
+              },
+              "policyNo": {
+                "displayName": "policyNo",
+                "type": "String",
+                "description": "policyNo of call"
+              },
+              "validUpto": {
+                "displayName": "validUpto",
+                "type": "String",
+                "description": "validUpto of call"
               }
             },
-            methods: {
-              sendMessage: {
-                displayName: "sendMessage",
-                type: "execute",
-                inputs: ["phonenr", "scenarioKey", "templateName", "templateVariables"],
-                requiredInputs: ["phonenr", "scenarioKey", "templateName", "templateVariables"],
-                requiredParameters: ["phonenr", "scenarioKey", "templateName", "templateVariables"],
-                outputs: ["result"],
-                data: {
-                  "httpMethod": "POST",
-                  "httpPath": "/advanced"
-                }
+            "methods": {
+              "getVahaanToken": {
+                "displayName": "Get Vahaan Token",
+                "type": "read",
+                "parameters": {
+                  "toke_url": {
+                    "displayName": "toke url",
+                    "type": "string"
+                  }
+                },
+                "requiredParameters": ["toke_url"],
+                "outputs": ["token"]
+              },
+              "checkAPIMethod": {
+                "displayName": "CheckAPIMethod",
+                "type": "read",
+                "parameters": {
+                  "methodUrl": {
+                    "displayName": "methodUrl",
+                    "description": "Method Url",
+                    "type": "string"
+                  },
+                  "authToken": {
+                    "displayName": "authToken",
+                    "description": "Auth Token",
+                    "type": "string"
+                  }
+                },
+                "requiredParameters": ["methodUrl", "authToken"],
+                "outputs": ["result"]
               }
             }
           }
@@ -74,8 +172,8 @@
       configuration
     }) {
       switch (objectName) {
-        case "message":
-          await onexecutemessage(methodName, parameters, properties, configuration);
+        case "VahaanIntegration":
+          await onexecuteVahaanIntegration(methodName, parameters, properties, configuration);
           break;
 
         default:
@@ -83,10 +181,14 @@
       }
     };
 
-    async function onexecutemessage(methodName, parameters, properties, configuration) {
+    async function onexecuteVahaanIntegration(methodName, parameters, properties, configuration) {
       switch (methodName) {
-        case "sendMessage":
-          await onexecutemessagesendMessage(parameters, properties, configuration);
+        case "getVahaanToken":
+          await onexecuteVahaanIntegrationgetVahaanToken(parameters, properties, configuration);
+          break;
+
+        case "checkAPIMethod":
+          await onexecuteVahaanIntegrationcheckAPIMethod(parameters);
           break;
 
         default:
@@ -94,27 +196,10 @@
       }
     }
 
-    function onexecutemessagesendMessage(parameters, properties, configuration) {
+    function onexecuteVahaanIntegrationgetVahaanToken(parameters, properties, configuration) {
       return new Promise((resolve, reject) => {
-        let urlValue = configuration["ServiceURL"];
-        let httpPath = `/advanced`;
-        let phonenr = properties["phonenr"];
-        let scenarioKey = properties["scenarioKey"];
-        let template = properties["templateName"];
-        let variable = properties["templateVariables"];
-        let data = {
-          "destinations": [{
-            "to": {
-              "phoneNumber": phonenr
-            }
-          }],
-          "scenarioKey": scenarioKey,
-          "whatsApp": {
-            "templateName": template,
-            "templateData": variable,
-            "language": "en"
-          }
-        };
+        let urlValue = parameters["toke_url"];
+        let httpPath = `grant_type=${configuration["grant_type"]}&client_id=${configuration["client_id"]}&client_secret=${configuration["client_secret"]}&username=${configuration["username"]}&password=${configuration["password"]}`;
         let xhr = new XMLHttpRequest();
 
         xhr.onreadystatechange = function () {
@@ -123,20 +208,77 @@
             if (xhr.status !== 200 && xhr.status !== 201) throw new Error("Failed with status " + xhr.status);
             let obj = JSON.parse(xhr.responseText);
             postResult({
-              result: xhr.responseText
+              "token": obj.access_token
             });
             resolve();
           } catch (error) {
-            console.log("ERROR: " + error);
             reject(error);
           }
         };
 
-        urlValue = urlValue.endsWith("/") ? urlValue : urlValue + "/";
-        httpPath = httpPath.startsWith("/") ? httpPath.substr(1) : httpPath + "/";
-        xhr.withCredentials = true;
-        xhr.open("POST", urlValue + httpPath);
+        xhr.withCredentials = false;
+        xhr.open("post", urlValue + "?" + httpPath);
         xhr.setRequestHeader("Content-Type", "application/json");
+        xhr.send(JSON.stringify(data));
+      });
+    }
+
+    function onexecuteVahaanIntegrationcheckAPIMethod(parameters, properties, configuration) {
+      return new Promise((resolve, reject) => {
+        let urlValue = parameters["methodUrl"];
+        let httpPath = parameters["authToken"];
+        let xhr = new XMLHttpRequest();
+
+        xhr.onreadystatechange = function (parameter) {
+          try {
+            if (xhr.readyState !== 4) return;
+            if (xhr.status !== 200 && xhr.status !== 201) throw new Error("Failed with status " + xhr.status);
+            let obj = JSON.parse(xhr.responseText);
+            postResult({
+              "message": obj.message
+            }, {
+              "status": obj.status
+            }, {
+              "registeringAuthority": obj.registeringAuthority
+            }, {
+              "registrationNo": obj.registrationNo
+            }, {
+              "registrationDate": obj.registrationDate
+            }, {
+              "ownerName": obj.ownerName
+            }, {
+              "fitnessUpto": obj.fitnessUpto
+            }, {
+              "taxUpto": obj.taxUpto
+            }, {
+              "rcStatus": obj.rcStatus
+            }, {
+              "nocDetails": obj.nocDetails
+            }, {
+              "financierName": obj.financierName
+            }, {
+              "financed": obj.financed
+            }, {
+              "blackListStatus": obj.blackListStatus
+            }, {
+              "permit": obj.permit
+            }, {
+              "insuranceName": obj.insuranceDetails.insuranceName
+            }, {
+              "policyNo": obj.insuranceDetails.policyNo
+            }, {
+              "validUpto": obj.insuranceDetails.validUpto
+            });
+            resolve();
+          } catch (error) {
+            reject(error);
+          }
+        };
+
+        xhr.withCredentials = false;
+        xhr.open("get", urlValue);
+        xhr.setRequestHeader("Content-Type", "application/json");
+        xhr.setRequestHeader("Authorization", "Bearer " + httpPath);
         xhr.send(JSON.stringify(data));
       });
     }
