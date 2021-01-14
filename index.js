02cb0563f529253f17cea425d363ebc98949f66a
mundra22 <44059307+mundra22@users.noreply.github.com>
2021-01-15 00:25:52 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,2 +1,177 @@
-!function(){function e(e){for(var t=e.split("\n"),r=[],a=t[0].split(","),s=1;s<t.length;s++){for(var i={},n=t[s].split(","),o=0;o<a.length;o++)i[a[o]]=n[o];r.push(i)}return JSON.stringify(r)}metadata={systemName:"jssp.TransportationCharges",displayName:"Transporatation Charges",description:"A K2 JSSP Broker to send Transporter Charger"},ondescribe=async function({configuration:e}){postSchema({objects:{message:{displayName:"message",description:"message",properties:{make:{displayName:"make",type:"string",description:"make"},makeModel:{displayName:"makeModel",type:"string",description:"makeModel Value"},year:{displayName:"year",type:"string",description:"year Name"},intercityTypeUrl:{displayName:"intercityTypeUrl",type:"string",description:"intercityTypeUrl values"},source:{displayName:"source",type:"string",description:"source variable values"},destination:{displayName:"destination",type:"string",description:"destination variable values"},transportationUrl:{displayName:"transportationUrl",type:"string",description:"transportationUrl variable values"}},methods:{extractIntercityType:{displayName:"extractIntercityType",type:"execute",inputs:["make","makeModel","year","intercityTypeUrl"],requiredInputs:["make","makeModel","year","intercityTypeUrl"],requiredParameters:["make","makeModel","year","intercityTypeUrl"],outputs:["result"]}}}}})},onexecute=async function({objectName:t,methodName:r,parameters:a,properties:s,configuration:i}){switch(t){case"message":await async function(t,r,a,s){switch(t){case"extractIntercityType":const r=await function(t,r,a){return new Promise((t,a)=>{var s=new XMLHttpRequest;s.onreadystatechange=function(){try{if(4!==s.readyState)return;if(200!==s.status&&201!==s.status)throw new Error("Failed with status "+s.status);intercityTypeCsv=s.responseText,intercityTypeJson=e(intercityTypeCsv);const a=JSON.parse(intercityTypeJson).find(e=>e.make===r.make&&e.makeModel===r.makeModel&&e.year===r.year);a.carIntercityType?t(a.carIntercityType):console.log("no intecityType found",a)}catch(e){a(e)}},s.open("GET",intercityTypeUrl),s.send()})}(0,a);await function(t,r,a,s){return new Promise((t,a)=>{var i=new XMLHttpRequest;i.onreadystatechange=function(){try{if(4!==i.readyState)return;if(200!==i.status&&201!==i.status)throw new Error("Failed with status "+i.status);intercityChargesCsv=i.responseText,intercityChargesJson=e(intercityChargesCsv);const a=JSON.parse(intercityChargesJson).find(e=>e.destination===r.destination&&e.source===r.source&&e.carIntercityType===s);a.amount?t(a.amount):console.log("no intecityType found",a),postResult({result:a.amount})}catch(e){a(e)}},i.open("GET",interCityChargesUrl),i.send()})}(0,a,0,r);break;default:throw new Error("The method "+t+" is not supported.")}}(r,0,s);break;default:throw new Error("The object "+t+" is not supported.")}}}();
+(function () {
+  metadata = {
+    "systemName": "jssp.TransportationCharges",
+    "displayName": "Transporatation Charges",
+    "description": "A K2 JSSP Broker to send Transporter Charger"
+  };
+
+  ondescribe = async function ({
+    configuration
+  }) {
+    postSchema({
+      objects: {
+        message: {
+          displayName: "message",
+          description: "message",
+          properties: {
+            make: {
+              displayName: "make",
+              type: "string",
+              description: "make"
+            },
+            makeModel: {
+              displayName: "makeModel",
+              type: "string",
+              description: "makeModel Value"
+            },
+            year: {
+              displayName: "year",
+              type: "string",
+              description: "year Name"
+            },
+            intercityTypeUrl: {
+              displayName: "intercityTypeUrl",
+              type: "string",
+              description: "intercityTypeUrl values"
+            },
+            source: {
+              displayName: "source",
+              type: "string",
+              description: "source variable values"
+            },
+            destination: {
+              displayName: "destination",
+              type: "string",
+              description: "destination variable values"
+            },
+            transportationUrl: {
+              displayName: "transportationUrl",
+              type: "string",
+              description: "transportationUrl variable values"
+            }
+          },
+          methods: {
+            extractIntercityType: {
+              displayName: "extractIntercityType",
+              type: "execute",
+              inputs: ["make", "makeModel", "year", "intercityTypeUrl", "source", "destination", "transportationUrl"],
+              requiredInputs: ["make", "makeModel", "year", "intercityTypeUrl", "source", "destination", "transportationUrl"],
+              requiredParameters: ["make", "makeModel", "year", "intercityTypeUrl", "source", "destination", "transportationUrl"],
+              outputs: ["result"]
+            }
+          }
+        }
+      }
+    });
+  };
+
+  onexecute = async function ({
+    objectName,
+    methodName,
+    parameters,
+    properties,
+    configuration
+  }) {
+    switch (objectName) {
+      case "message":
+        await onexecutemessage(methodName, parameters, properties, configuration);
+        break;
+
+      default:
+        throw new Error("The object " + objectName + " is not supported.");
+    }
+  };
+
+  async function onexecutemessage(methodName, parameters, properties, configuration) {
+    switch (methodName) {
+      case "extractIntercityType":
+        const intercityType = await onexecutemessageextractIntercityType(parameters, properties);
+        const charge = await onexecutemessagegetCharge(parameters, properties, configuration, intercityType);
+        break;
+
+      default:
+        throw new Error("The method " + methodName + " is not supported.");
+    }
+  }
+
+  function onexecutemessageextractIntercityType(parameters, properties, configuration) {
+    return new Promise((resolve, reject) => {
+      var xhrIntercityType = new XMLHttpRequest();
+
+      xhrIntercityType.onreadystatechange = function () {
+        try {
+          if (xhrIntercityType.readyState !== 4) return;
+          if (xhrIntercityType.status !== 200 && xhrIntercityType.status !== 201) throw new Error("Failed with status " + xhrIntercityType.status);
+          intercityTypeCsv = xhrIntercityType.responseText;
+          intercityTypeJson = csvJSON(intercityTypeCsv);
+          intercityTypeUrl = properties["intercityTypeUrl"];
+          const car = JSON.parse(intercityTypeJson).find(e => e.make === properties["make"] && e.makeModel === properties["makeModel"] && e.year === properties["year"]);
+
+          if (car.carIntercityType) {
+            resolve(car.carIntercityType);
+          } else {
+            console.log('no intecityType found', car);
+          }
+        } catch (error) {
+          reject(error);
+        }
+      };
+
+      xhrIntercityType.open("GET", intercityTypeUrl);
+      xhrIntercityType.send();
+    });
+  }
+
+  function onexecutemessagegetCharge(parameters, properties, configuration, intercityType) {
+    return new Promise((resolve, reject) => {
+      var xhrIntercityCharges = new XMLHttpRequest();
+
+      xhrIntercityCharges.onreadystatechange = function () {
+        try {
+          if (xhrIntercityCharges.readyState !== 4) return;
+          if (xhrIntercityCharges.status !== 200 && xhrIntercityCharges.status !== 201) throw new Error("Failed with status " + xhrIntercityCharges.status);
+          intercityChargesCsv = xhrIntercityCharges.responseText;
+          intercityChargesJson = csvJSON(intercityChargesCsv);
+          interCityChargesUrl = properties["transportationUrl"];
+          const match = JSON.parse(intercityChargesJson).find(e => e.destination === properties["destination"] && e.source === properties["source"] && e.carIntercityType === intercityType);
+
+          if (match.amount) {
+            resolve(match.amount);
+          } else {
+            console.log('no intecityType found', match);
+          }
+
+          postResult({
+            "result": match.amount
+          });
+        } catch (error) {
+          reject(error);
+        }
+      };
+
+      xhrIntercityCharges.open("GET", interCityChargesUrl);
+      xhrIntercityCharges.send();
+    });
+  }
+
+  function csvJSON(csv) {
+    var lines = csv.split("\n");
+    var result = [];
+    var headers = lines[0].split(",");
+
+    for (var i = 1; i < lines.length; i++) {
+      var obj = {};
+      var currentline = lines[i].split(",");
+
+      for (var j = 0; j < headers.length; j++) {
+        obj[headers[j]] = currentline[j];
+      }
+
+      result.push(obj);
+    }
+
+    return JSON.stringify(result);
+  }
+
+}());
 //# sourceMappingURL=index.js.map
