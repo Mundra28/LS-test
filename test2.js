9e4ad52b7090330cfd3fb2e39a7a2e73e9c749d1
mundra22 <44059307+mundra22@users.noreply.github.com>
2021-04-24 15:13:39 +0530

    Update test2.js

--- test2.js
+++ test2.js
@@ -43,187 +43,177 @@
               },
               "type": {
                 "displayName": "type",
-                "type": "String",
+                "type": "string",
                 "description": "type of call"
               },
               "Name": {
                 "displayName": "Name",
-                "type": "String",
+                "type": "string",
                 "description": "Name of call"
               },
               "Primary_Mobile__c": {
                 "displayName": "Primary_Mobile__c",
-                "type": "String",
+                "type": "string",
                 "description": "Primary_Mobile__c"
               },
               "Email__c": {
                 "displayName": "Email__c",
-                "type": "String",
+                "type": "string",
                 "description": "Email__c"
               },
               "BillingStreet": {
                 "displayName": "BillingStreet",
-                "type": "String",
+                "type": "string",
                 "description": "BillingStreet"
               },
               "BillingCity": {
                 "displayName": "BillingCity",
-                "type": "String",
+                "type": "string",
                 "description": "BillingCity"
               },
               "BillingState": {
                 "displayName": "BillingState",
-                "type": "String",
+                "type": "string",
                 "description": "BillingState"
               },
               "BillingPostalCode": {
                 "displayName": "BillingPostalCode",
-                "type": "String",
+                "type": "string",
                 "description": "BillingPostalCode"
               },
               "BillingCountry": {
                 "displayName": "BillingCountry",
-                "type": "String",
+                "type": "string",
                 "description": "BillingCountry"
               },
               "CMC_City_Mapping__c": {
                 "displayName": "CMC_City_Mapping__c",
-                "type": "String",
+                "type": "string",
                 "description": "CMC_City_Mapping__c of call"
               },
               "Eligible_in_TCS__c": {
                 "displayName": "Eligible_in_TCS__c",
-                "type": "String",
+                "type": "string",
                 "description": "Eligible_in_TCS__c"
               },
               "X6_Digit_FCG_Id__c": {
                 "displayName": "X6_Digit_FCG_Id__c",
-                "type": "String",
+                "type": "string",
                 "description": "X6_Digit_FCG_Id__c of call"
               },
               "Total_Payment_Received_for_Onboarding__c": {
                 "displayName": "Total_Payment_Received_for_Onboarding__c",
-                "type": "String",
+                "type": "string",
                 "description": "Total_Payment_Received_for_Onboarding__c of call"
               },
               "RC_Transfer_Count_Post_90_Days_Delivery__c": {
                 "displayName": "RC_Transfer_Count_Post_90_Days_Delivery__c",
-                "type": "String",
+                "type": "string",
                 "description": "RC_Transfer_Count_Post_90_Days_Delivery__c of call"
               },
               "Dealer_Inventory_More_Than_90_Days__c": {
                 "displayName": "Dealer_Inventory_More_Than_90_Days__c",
-                "type": "String",
+                "type": "string",
                 "description": "Dealer_Inventory_More_Than_90_Days__c of call"
               },
               "Forfeiture_Applicable__c": {
                 "displayName": "Forfeiture_Applicable__c",
-                "type": "String",
+                "type": "string",
                 "description": "Forfeiture_Applicable__c of call"
               },
               "Dealer_Financing_Eligible__c": {
                 "displayName": "Dealer_Financing_Eligible__c",
-                "type": "String",
-                "description": "Dealer_Financing_Eligible__c of call"
-              },
-              "Forfeiture_Applicable__c": {
-                "displayName": "Forfeiture_Applicable__c",
-                "type": "String",
-                "description": "Forfeiture_Applicable__c of call"
-              },
-              "Dealer_Financing_Eligible__c": {
-                "displayName": "Dealer_Financing_Eligible__c",
-                "type": "String",
+                "type": "string",
                 "description": "Dealer_Financing_Eligible__c of call"
               },
               "Id": {
                 "displayName": "Id",
-                "type": "String",
+                "type": "string",
                 "description": "Id of call"
               },
               "Public_URL__c": {
                 "displayName": "Public_URL__c",
-                "type": "String",
+                "type": "string",
                 "description": "Public_URL__c of call"
               },
               "Name_on_Cancelled_Cheque__c": {
                 "displayName": "Name_on_Cancelled_Cheque__c",
-                "type": "String",
+                "type": "string",
                 "description": "Name_on_Cancelled_Cheque__c of call"
               },
               "Notional_Credit__c": {
                 "displayName": "Notional_Credit__c",
-                "type": "String",
+                "type": "string",
                 "description": "Notional_Credit__c of call"
               },
               "IFSC_code_on_Cancelled_Cheque__c": {
                 "displayName": "IFSC_code_on_Cancelled_Cheque__c",
-                "type": "String",
+                "type": "string",
                 "description": "IFSC_code_on_Cancelled_Cheque__c of call"
               },
               "Bank_Account_Number__c": {
                 "displayName": "Bank_Account_Number__c",
-                "type": "String",
+                "type": "string",
                 "description": "Bank_Account_Number__c of call"
               },
               "Home_Delivery_Status__c": {
                 "displayName": "Home_Delivery_Status__c",
-                "type": "String",
+                "type": "string",
                 "description": "Home_Delivery_Status__c of call"
               },
               "FCG_Id__c": {
                 "displayName": "FCG_Id__c",
-                "type": "String",
+                "type": "string",
                 "description": "FCG_Id__c of call"
               },
               "Purpose_of_Payment__c": {
                 "displayName": "Purpose_of_Payment__c",
-                "type": "String",
+                "type": "string",
                 "description": "Purpose_of_Payment__c of call"
               },
               "CurrencyISOCode": {
                 "displayName": "CurrencyISOCode",
-                "type": "String",
+                "type": "string",
                 "description": "CurrencyISOCode of call"
               },
               "RecordTypeId": {
                 "displayName": "RecordTypeId",
-                "type": "String",
+                "type": "string",
                 "description": "RecordTypeId of call"
               },
               "Payment_Processed_Date__c": {
                 "displayName": "Payment_Processed_Date__c",
-                "type": "String",
+                "type": "string",
                 "description": "Payment_Processed_Date__c of call"
               },
               "Amount__c": {
                 "displayName": "Amount__c",
-                "type": "String",
+                "type": "string",
                 "description": "Amount__c of call"
               },
               "Finance_Comments__c": {
                 "displayName": "Finance_Comments__c",
-                "type": "String",
+                "type": "string",
                 "description": "Finance_Comments__c of call"
               },
               "Payment_Receive_Date__c": {
                 "displayName": "Payment_Receive_Date__c",
-                "type": "String",
+                "type": "string",
                 "description": "Payment_Receive_Date__c of call"
               },
               "Payment_Received_Date_and_Time__c": {
                 "displayName": "Payment_Received_Date_and_Time__c",
-                "type": "String",
+                "type": "string",
                 "description": "Payment_Received_Date_and_Time__c of call"
               },
               "LS_ID__c": {
                 "displayName": "LS_ID__c",
-                "type": "String",
+                "type": "string",
                 "description": "LS_ID__c of call"
               },
               "result": {
                 "displayName": "result",
-                "type": "String",
+                "type": "string",
                 "description": "result of call"
               }
             },
@@ -320,8 +310,7 @@
     async function onexecuteSalesforceIntegration(methodName, parameters, properties, configuration) {
       switch (methodName) {
         case "generateToken":
-          var tok = await onexecuteSalesforceIntegrationgenerateToken(parameters, properties, configuration);
-          await onexecuteSalesforceIntegrationaccountDetails(parameters, properties, configuration, tok);
+          await onexecuteSalesforceIntegrationgenerateToken(parameters, properties, configuration);
           break;
 
         case "accountDetails":
@@ -345,19 +334,19 @@
     function onexecuteSalesforceIntegrationgenerateToken(parameters, properties, configuration) {
       return new Promise((resolve, reject) => {
         let urlValue = parameters["toke_url"];
-        let httpPath = encodeURIComponent(`grant_type=${configuration["grant_type"]}&client_id=${configuration["client_id"]}&client_secret=${configuration["client_secret"]}&username=${configuration["username"]}&password=${configuration["password"]}`);
+        let httpPath = encodeURI(`grant_type=${configuration["grant_type"]}&client_id=${configuration["client_id"]}&client_secret=${configuration["client_secret"]}&username=${configuration["username"]}&password=${configuration["password"]}`);
         let xhr = new XMLHttpRequest();
         console.log(httpPath);
 
         xhr.onreadystatechange = function () {
           try {
             if (xhr.readyState !== 4) return;
-            if (xhr.status !== 200 && xhr.status !== 201) throw new Error("Failed with status " + xhr.status);
+            if (xhr.status !== 200 && xhr.status !== 201) throw new Error("Failed with status " + xhr.status + httpPath);
             let obj = JSON.parse(xhr.responseText);
             postResult({
               "token": obj.access_token
             });
-            resolve(obj.access_token);
+            resolve();
           } catch (error) {
             reject(error);
           }
